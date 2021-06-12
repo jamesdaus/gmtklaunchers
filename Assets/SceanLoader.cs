a4cd2155f67301a2831fd12da1ba3a36fa04acc3
ChuckinAllen <Richardposch11011@gmail.com>
2021-06-11 19:33:19 -0500

    Menu settings button

---
+++ Assets/SceanLoader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MrushumeMan.SeanManagement
+{
+    public class SceanLoader : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        public void LoadMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void LoadSettings()
+        {
+
+        }
+
+        public void LoadCredits()
+        {
+
+        }
+
+        public void LoadNextScean()
+        {
+
+        }
+    }
+}
+
